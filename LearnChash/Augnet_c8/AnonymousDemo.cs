64dd43324bd2fbf0c30cc15ab9ca934f7905162c
prathik hegde <prathikch1998@gmail.com>
2024-10-17 17:32:41 -0700

    augnet_8

---
+++ LearnChash/Augnet_c8/AnonymousDemo.cs
@@ -0,0 +1,35 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace LearnChash.Augnet_c8
+//{
+//    delegate int CountIt(int end);
+//    class AnonymousDemo
+//    {
+
+
+//        static void Main(string[] args)
+//        {
+//            CountIt count = delegate(int end)
+//            {
+//                int sum = 0;
+//                for (int i = 0; i < end; i++)
+//                {
+//                    {
+//                        Console.WriteLine("i is "+i);
+//                        sum += i;
+//                        Console.WriteLine("sum after this i is "+sum);
+//                    }
+//                }
+//                return sum;
+//            };
+
+//            Console.WriteLine("SUM "+count(4));
+
+//        }
+
+//            }
+//         }
---
+++ LearnChash/Augnet_c8/ExceptionDemo.cs
@@ -0,0 +1,67 @@
+﻿//using System;
+
+//namespace LearnChash.Augnet_c8
+//{
+//    class ExceptionDemo
+//    {
+//        class CustomeException : Exception
+//        {
+//            public CustomeException(string message, Exception innerException) : base(message, innerException) { }
+
+//            public override string Message
+//            {
+//                get
+//                {
+//                    return "Divisor cannot be zero: " + base.Message;
+//                }
+//            }
+
+//            public override string HelpLink
+//            {
+//                get
+//                {
+//                    return "Get more help: https://www.google.com";
+//                }
+//            }
+//        }
+
+//        static void Main(string[] args)
+//        {
+//            int num1, num2, num3;
+//            try
+//            {
+//                Console.WriteLine("Enter 3 numbers");
+//                num1 = int.Parse(Console.ReadLine());
+//                num2 = int.Parse(Console.ReadLine());
+
+//                //if (num2 == 0)
+//                //{
+//                //    // Custom exception with inner exception
+//                //    throw new CustomeException("Cant be zero!!", new ArgumentException("Divisor was zero."));
+//                //}
+
+//                num3 = num1 / num2;
+//                Console.WriteLine(num3);
+//            }
+//            catch (CustomeException ex)
+//            {
+//                Console.WriteLine(ex.Message);
+//                Console.WriteLine("Inner Exception: " + ex.InnerException.Message);
+//            }
+//            catch (FormatException ex)
+//            {
+//                Console.WriteLine("Input was not in the correct format: " + ex.Message);
+//            }
+//            catch (Exception e)
+//            {
+//                Console.WriteLine(e.Message);
+//            }
+//            finally
+//            {
+//                Console.WriteLine("Finally Done");
+//            }
+
+//            Console.WriteLine("Done now");
+//        }
+//    }
+//}
