a83bf7d9fc56991afa7d2ac2ae1952f0ac099940
prathik hegde <prathikch1998@gmail.com>
2024-10-17 18:42:01 -0700

    aug9 half done

--- LearnChash/Augnet_c8/LambdaDemo.cs
+++ LearnChash/Augnet_c8/LambdaDemo.cs
@@ -1,75 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace LearnChash.Augnet_c8
-{
-    internal class LambdaDemo
-    {
-        delegate int Incr(int i);
-        delegate bool IsEvent(int v);
-        delegate bool IsRange(int low, int high,int value);
-        delegate int Intop(int end);
-        static void Main(string[] args)
-        {
-            //(param list)=> expr
+//namespace LearnChash.Augnet_c8
+//{
+//    internal class LambdaDemo
+//    {
+//        delegate int Incr(int i);
+//        delegate bool IsEvent(int v);
+//        delegate bool IsRange(int low, int high,int value);
+//        delegate int Intop(int end);
+//        static void Main(string[] args)
+//        {
+//            //(param list)=> expr
 
-            Incr incr = count => count + 2;
-            int x = -10;
-            while(x<= 0)
-            {
-                Console.WriteLine(x);
-                x = incr(x);
+//            Incr incr = count => count + 2;
+//            int x = -10;
+//            while(x<= 0)
+//            {
+//                Console.WriteLine(x);
+//                x = incr(x);
 
-            }
+//            }
 
-            //iseven lambda
+//            //iseven lambda
 
-            IsEvent even = n => n % 2 == 0;
-            for (int i = 0; i < 10; i++)
-            {
-                if (even(i))
-                {
-                    Console.WriteLine(i);
-                }
-            }
+//            IsEvent even = n => n % 2 == 0;
+//            for (int i = 0; i < 10; i++)
+//            {
+//                if (even(i))
+//                {
+//                    Console.WriteLine(i);
+//                }
+//            }
 
-            //isrange lambda
+//            //isrange lambda
 
-            IsRange rangeno = (low, high, value) => (value >= low && value <= high);
-            for (int i = 0; i < 10; i++)
-            {
-                if (rangeno(2, 8, i))
-                {
-                    Console.WriteLine("in Range " + i);
-                }
-                else
-                {
-                    Console.WriteLine("Not in range "+i);
-                }
-            }
+//            IsRange rangeno = (low, high, value) => (value >= low && value <= high);
+//            for (int i = 0; i < 10; i++)
+//            {
+//                if (rangeno(2, 8, i))
+//                {
+//                    Console.WriteLine("in Range " + i);
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Not in range "+i);
+//                }
+//            }
 
-            //factorial
-            Intop fact = n =>
-            {
-                int r = 1;
-                if (n == 0 || n == 1)
-                {
-                    return 1;
-                }
-                else
-                {
-                    for (int i = 2; i <= n; i++)
-                    {
-                        r = r * i;
-                    }
-                    return r;
-                }
-            };
-            Console.WriteLine(fact(10));
+//            //factorial
+//            Intop fact = n =>
+//            {
+//                int r = 1;
+//                if (n == 0 || n == 1)
+//                {
+//                    return 1;
+//                }
+//                else
+//                {
+//                    for (int i = 2; i <= n; i++)
+//                    {
+//                        r = r * i;
+//                    }
+//                    return r;
+//                }
+//            };
+//            Console.WriteLine(fact(10));
 
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ LearnChash/Augnet_c9/DelegatesGeneric.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnChash.Augnet_c9
+{
+
+    /* 3 types of built in generic delegates
+     * 1.func(system namespace, one or more params upto 16,returns out param,yield keyword)
+     * 2.action(one or more params upto 16, returns nothing)
+     * 3.predicate(one param, returns bool)
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+    internal class DelegatesGeneric
+    {
+        static void Main(string[] args)
+        {
+            //Func<int, float, double, double> obj = new Func<int, float, double, double>(AddNumber);
+            //double res = obj.Invoke(100, 125.3f, 456.678);
+            //Console.WriteLine(res);
+
+            //write in reg
+            Func<int, float, double, double> obj = (x, y, z) =>
+            {
+                return x + y + z;
+            };
+            double res = obj.Invoke(100, 125.3f, 456.678);
+            Console.WriteLine(res);
+
+
+            //Action<int, float, double> obj2 = new Action<int, float, double>(AddNumber2);
+            //obj2.Invoke(100, 125.3f, 456.678);
+
+            Action<int, float, double> obj2 = (x, y, z) =>
+            {
+                Console.WriteLine(x + y + z);
+            };
+            obj2.Invoke(100, 125.3f, 456.678);
+
+            //Predicate<string> obj3 = new Predicate<string>(CheckLength);
+            //bool result = obj3.Invoke("pRATHIK");
+            //Console.WriteLine(result);
+
+            Predicate<string> obj3 = (name) => {
+                return name.Length > 5;
+            };
+            bool result = obj3.Invoke("pRATHIK");
+            Console.WriteLine(result);
+
+
+        }
+
+        private static bool CheckLength(string name)
+        {
+            if(name.Length > 5)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static void AddNumber2(int arg1, float arg2, double arg3)
+        {
+            Console.WriteLine(arg1+arg2+arg3);
+        }
+
+        private static double AddNumber(int arg1, float arg2, double arg3)
+        {
+            return arg1 + arg2 + arg3;
+        }
+    }
+}
