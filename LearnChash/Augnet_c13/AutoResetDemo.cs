99774b5bc9f13797526939e6804ee3ffe7e283bd
PRATHIK CHANDRA HEGDE <prathikch1998@gmail.com>
2024-10-27 11:56:07 -0500

    Add files via upload

---
+++ LearnChash/Augnet_c13/AutoResetDemo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnChash.Augnet_c13
+{
+    internal class AutoResetDemo
+    {
+        static ManualResetEvent autoresetevent = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Thread th = new Thread(SomeMethod);
+            //    th.Start();
+
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.ReadLine();
+            //    autoresetevent.Set();
+            //}
+
+            Thread th = new Thread(SomeMethod);
+            th.Start();
+            Thread.Sleep(2000);
+            autoresetevent.Set();
+
+
+
+        }
+        static void SomeMethod()
+        {
+            Console.WriteLine("starting ....................");
+            autoresetevent.WaitOne();
+            Console.WriteLine();
+            autoresetevent.WaitOne();
+            Console.WriteLine("finishing ....................");
+           
+
+        }
+    }
+}
---
+++ LearnChash/Augnet_c13/ThreadPoolDemo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace LearnChash.Augnet_c13
+{
+    internal class ThreadPoolDemo
+    {
+        static void Main(string[] args)
+        {
+            // Measure and display time for ThreadPool
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < 5; i++)
+            {
+                ThreadPool.SetMinThreads(1,2);
+                ThreadPool.SetMaxThreads(1,2);
+                ThreadPool.QueueUserWorkItem(MyMethod, i);
+                Console.WriteLine($"ThreadPool Thread Count: {ThreadPool.ThreadCount}");
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Elapsed time for ThreadPool: {stopwatch.ElapsedMilliseconds} ms");
+
+            Console.WriteLine("________________________");
+
+            // Measure and display time for Thread
+            stopwatch.Restart();
+            for (int i = 0; i < 5; i++)
+            {
+                Thread thread = new Thread(MyMethod)
+                {
+                    Name = "Thread: " + i
+                };
+                thread.Start(i);
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Elapsed time for Threads: {stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        public static void MyMethod(object obj)
+        {
+            Thread th = Thread.CurrentThread;
+            string message = $"Background: {th.IsBackground}, Thread Pool: {th.IsThreadPoolThread}, " +
+                             $"Thread ID: {th.ManagedThreadId}, Parameter: {obj}";
+            Console.WriteLine(message);
+        }
+    }
+}
